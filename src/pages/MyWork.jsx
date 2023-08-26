130b9e9355c7f2c4c326f657a55cc4212ff3b8ae
c3sar-0 <aa4549807@gmail.com>
2023-08-25 23:08:49 -0300

    added demo images and prepared for gh pages deploy

--- src/pages/MyWork.jsx
+++ src/pages/MyWork.jsx
@@ -1,3 +1,6 @@
+import bb1 from "../assets/bb1.png"
+import bb2 from "../assets/bb2.png"
+
 const MyWork = () => {
   return (
     <>
@@ -5,9 +8,13 @@ const MyWork = () => {
         <section className="my-work__section my-work__bytebusters">
           <div className="my-work__info">
             <h1 className="my-work__title">BYTE BUSTERS</h1>
+            <p className="my-work__desc">
+              Bytebusters is a website where users can register, join, share and interact with posts. Here everyone can let their creativity free and enjoy while sharing experiences with the community. Create, like and comment posts, interact with other users, make friends, and much more!
+            </p>
           </div>
           <div className="my-work__images">
-            <img className="my-work__desktop-img" alt="demo desktop image" />
+            <img className="my-work__desktop-img" alt="demo desktop image" src={bb1} />
+            <img className="my-work__mobile-img" alt="demo mobile image" src={bb2} />
           </div>
         </section>
       </div>
